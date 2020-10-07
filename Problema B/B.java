ed8184d50a9cac91ac5fe3249bf2b33a0113bcd7
Polaryti <mario.identation@gmail.com>
2020-10-07 16:59:26 +0200

    A

---
+++ Problema B/B.java
@@ -0,0 +1,37 @@
+import java.io.PrintWriter;
+import java.util.*;
+
+public class A {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    PrintWriter pw = new PrintWriter(System.out);
+
+    int n = Integer.parseInt(sc.nextLine().trim());
+    List<Integer> x = new LinkedList<>();
+    
+    while(sc.hasNext()) {
+      x.add(sc.nextInt());
+    }
+
+    pw.print(func(n, x));
+
+    sc.close();
+    pw.close();
+
+  }
+
+  public static int func(int n, List<Integer> x) {
+    int[] cache = new int[n + 1];
+    cache[0] = 1;
+    
+    for (int i = 0; i < n + 1; i++) {
+      for (Integer aux : x) {
+        if (i - aux >= 0) {
+          cache[i] += cache[i - aux];
+        }
+      }
+    }
+    
+    return cache[n];
+  }
+}
