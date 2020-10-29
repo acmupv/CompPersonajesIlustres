b0287a46ec2d92dea983e7bf8430f5f1e71fe951
Mario Campos Mocholí <mario.identation@gmail.com>
2020-10-29 21:33:58 +0100

    Update J.py

--- Problema J/J.py
+++ Problema J/J.py
@@ -1,19 +1,13 @@
-def Jsol(prices=[]; k=0)
-    sum  = 0; max = 0; min = 0; fl = 1
-    Mval = 0; Mmin = 0; Mmax = -1
-    while max < len(prices) or not fl:
-        if fl:
-            sum += prices[max]
-        if sum <= k:
-            if sum > Mval or (sum == Mval and max - min > Mmax - Mmin):
-                Mval=sum
-                Mmin=min
-                Mmax=max
-            max += 1
-            fl = 1
-        if sum >= k:
-            fl = 0
-            if min <= max:
-                sum -= prices[min]
-                min += 1   
-    return(Mmax - Mmin + 1)
+def func(prices):
+    If min(prices)>budget:
+    	return 0
+
+    prices = [0] + prices + [0]
+    for L in range(len(prices) - 2, 1, -1):
+        S = sum(prices[1:L + 1])
+        for i in range(1, len(prices) - L):
+            S = S - prices[i - 1] + prices[i + L]
+            if S == budget:
+                return (L + 1)
+
+    return 0
